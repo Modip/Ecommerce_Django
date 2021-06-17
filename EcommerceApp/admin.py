69a3b7ee8f1198fbd80e97338c953b5d8c2dd555
Mor Diop <mordip06@gmail.com>
2021-06-17 16:39:30 +0000

    Creation des models

--- EcommerceApp/admin.py
+++ EcommerceApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from EcommerceApp.models import Categories, SubCategories
+
+admin.site.register(Categories)
+admin.site.register(SubCategories)
